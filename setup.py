d2cc4fb3a414d1d9aa6946cc856d6879f079acfb
Dan Dyer <dan.dyer00@gmail.com>
2016-10-10 13:32:49 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(name='testApp',
+      version='1.0',
+      description='Test App',
+      author='Dan Dyer',
+      author_email='dan.dyer00@gmail.com',
+      url='https://github.com/dandyer00/test',
+      packages=['testApp', 'tests'],
+     )
